5e50cd2ab4109624bd0a5aa95e408bc060d37a0e
arsonal <897386402@qq.com>
2020-03-30 21:31:20 +0800

    grep file

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,12 +1,38 @@
 package com.github.hcsp.io;
 
-import java.io.File;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {}
+    public static int grep(File target, String text) {
+        List<String> list = new ArrayList<>();
+        try (InputStreamReader isr = new InputStreamReader(new FileInputStream(target), Charset.forName("UTF-8"));
+             BufferedReader reader = new BufferedReader(isr)) {
+            String line = reader.readLine();
+            while (line != null) {
+                list.add(line);
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new IllegalArgumentException("文件未找到或无法被读取");
+        } catch (OutOfMemoryError oom) {
+            throw new IllegalArgumentException("文件太大导致OOM");
+        }
+        if (list.isEmpty()) {
+            return -1;
+        }
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).contains(text)) {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
