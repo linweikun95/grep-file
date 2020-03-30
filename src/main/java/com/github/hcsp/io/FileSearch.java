79f576e2979d843f7acd28fb6d038e7690a8b23e
arsonal <897386402@qq.com>
2020-03-30 21:40:20 +0800

    grep file, no oom Exception

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,36 +1,31 @@
 package com.github.hcsp.io;
 
-import java.io.*;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
     public static int grep(File target, String text) {
-        List<String> list = new ArrayList<>();
-        try (InputStreamReader isr = new InputStreamReader(new FileInputStream(target), Charset.forName("UTF-8"));
+
+        try (FileReader isr = new FileReader(target);
              BufferedReader reader = new BufferedReader(isr)) {
             String line = reader.readLine();
+            int lineNo = 0;
             while (line != null) {
-                list.add(line);
+                lineNo++;
+                if (line.contains(text)) {
+                    return lineNo;
+                }
                 line = reader.readLine();
             }
         } catch (IOException e) {
             throw new IllegalArgumentException("文件未找到或无法被读取");
-        } catch (OutOfMemoryError oom) {
-            throw new IllegalArgumentException("文件太大导致OOM");
         }
-        if (list.isEmpty()) {
-            return -1;
-        }
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).contains(text)) {
-                return i + 1;
-            }
-        }
+
         return -1;
     }
 
