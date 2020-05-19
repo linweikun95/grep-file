b41a2d34cb05de33ab30429bc3507fc93cc53deb
你的英文名 <>
2020-05-19 21:24:14 +0800

    try 4

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
-    // 请不要让这个方法抛出checked exception
+    // 请不要让这个方法抛出checked exception.
     public static int grep(File target, String text) {
         try {
             BufferedReader buf = new BufferedReader(new FileReader(target));
