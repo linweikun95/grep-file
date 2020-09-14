64936ae5c501c47601ad76de6216390be0b65adb
godvvvvvv <88508lgj@163.com>
2020-09-14 19:20:33 +0800

    查找对应字符串所在行的行号

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,12 +1,36 @@
 package com.github.hcsp.io;
 
-import java.io.File;
+import javax.imageio.IIOException;
+import javax.swing.text.html.parser.Entity;
+import java.io.*;
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {}
+    public static int grep(File target, String text) {
+        boolean judge = target.canRead() && target.isFile() && target.exists();
+        if (!judge) {
+            throw new IllegalArgumentException();
+        } else {
+            try {
+                FileReader reader = new FileReader(target);
+                BufferedReader bufferedReader = new BufferedReader(reader);
+                int count = 1;
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    if (line.contains(text)) {
+                        return count;
+                    } else {
+                        count++;
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return -1;
+    }
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
